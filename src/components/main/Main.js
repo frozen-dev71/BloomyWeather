4f8d1b126327fe18ae7febad1df6d1ebb847e0de
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-12-31 01:25:03 +0300

    Implemented BottomContainer Styling

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,11 +2,13 @@ import Main from "./Main.styled";
 import TopContainer from "../top-container/TopContainer";
 import CenterContainer from "../center-container/CenterContainer";
 
+
 const WeatherContainer = ({ todayWeather, handleSubmit, handleValidation, formValue, formError , loading, noData}) => {
     return (
         <Main>
         <TopContainer formError={formError} handleSubmit={handleSubmit} handleValidation={handleValidation} formValue={formValue} />
         <CenterContainer todayWeather={todayWeather} loading={loading} noData={noData}/>
+
         </Main>
     )
 }
