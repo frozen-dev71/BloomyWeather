d993457cc9e198787a326f14e70b1d6237c31517
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-12-31 01:27:46 +0300

    Implemented Bottom into Main component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,7 @@
 import Main from "./Main.styled";
 import TopContainer from "../top-container/TopContainer";
 import CenterContainer from "../center-container/CenterContainer";
+import BottomContainer from "../bottom-container/BottomContainer";
 
 
 const WeatherContainer = ({ todayWeather, handleSubmit, handleValidation, formValue, formError , loading, noData}) => {
@@ -8,7 +9,7 @@ const WeatherContainer = ({ todayWeather, handleSubmit, handleValidation, formVa
         <Main>
         <TopContainer formError={formError} handleSubmit={handleSubmit} handleValidation={handleValidation} formValue={formValue} />
         <CenterContainer todayWeather={todayWeather} loading={loading} noData={noData}/>
-
+        <BottomContainer todayWeather={todayWeather} loading={loading} noData={noData}/>
         </Main>
     )
 }
