0bad605584516ea8dc7a1af6344880a258b4997b
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-12-31 00:28:41 +0300

    Added react-router and react spinner packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
