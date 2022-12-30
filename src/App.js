081f5710a88254d765e37b044380335abca51c6a
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-12-31 00:32:38 +0300

    Added fetch weather data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [todayWeather, setTodayWeather] = useState({ name: "", country: "", temp: "", icon: "03d", weather: "", weatherDesc: "", feelsLike: "", humidity: "", wind: "", highest: "", lowest: "" });
+  const [searchedLocation, setSearchedLocation] = useState("Buenos Aires");
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
+  const [searchDone, setSearchDone] = useState(false);
+  
+  
+  useEffect(() => {
+    
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchedLocation}&limit=1&appid=aa3b70851ff3cfaab595923162142fe3
+`)
+      .then(response => response.json())
+      .then(data => {
+        setLat(data[0]?.lat);
+        setLon(data[0]?.lon);
+        setSearchDone(true);
+        setTodayWeather(prev => { return { ...prev, name: data[0]?.local_names?.en, country: data[0]?.country } })
+       
+      }).catch((err) => {
+        console.log(err.message);
+      });
+
+  }, [searchedLocation]);
+
+  
   return (
     <div className="App">
       <header className="App-header">
