c44b81bd0237ee26262d2fde56b6e21bc828a42e
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-12-31 01:00:28 +0300

    Added global and index stylings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { useEffect, useState } from "react";
-import './App.css';
 import { ThemeProvider } from "styled-components";
 
 function App() {
