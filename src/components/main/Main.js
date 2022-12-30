b5bc0afd435df828145cf47af1622793c14927e7
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-12-31 01:19:52 +0300

    Implemented top container into main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,10 @@
 import Main from "./Main.styled";
+import TopContainer from "../top-container/TopContainer";
+
 const WeatherContainer = ({ todayWeather, handleSubmit, handleValidation, formValue, formError , loading, noData}) => {
     return (
         <Main>
+        <TopContainer formError={formError} handleSubmit={handleSubmit} handleValidation={handleValidation} formValue={formValue} />
 
         </Main>
     )
